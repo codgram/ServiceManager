553a9e0901f771171ef108b7af598130492e67ea
kevorkkeheian <kevork.keheian@codgram.com>
2020-12-08 23:34:20 +0200

    ✨[Service] add style service

---
+++ src/Client/Services/Style.cs
@@ -0,0 +1,19 @@
+namespace ServiceManager.Client.Services
+{
+    public class Style
+    {
+        //public string Height { get; set; }
+
+        public string GetHeight(int count) {
+            if(count < 3) {
+                return "100px";
+            }
+            else if(count < 5) {
+                return "200px";
+            }
+            else {
+                return "300px";
+            }
+        }
+    }
+}
